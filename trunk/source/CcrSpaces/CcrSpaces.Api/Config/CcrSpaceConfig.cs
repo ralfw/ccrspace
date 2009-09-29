fa554317e568d80f918b58eef6f257c1f79200e3
RalfW08 <RalfW08@1f96fe94-a9f5-11de-a86f-b10fb6d3a35f>
2009-09-29 11:29:15 +0000

    Finish folder renaming. Separated tests defining the API and true tests. First test for creating a simple standalone listener.

---
+++ trunk/source/CcrSpaces/CcrSpaces.Api/Config/CcrSpaceConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CcrSpaces.Api.Config
+{
+    public class CcrSpaceConfig
+    {
+        public static CcrSpaceConfig New()
+        {
+            return new CcrSpaceConfig();
+        }
+
+
+        public CcrSpaceConfig RunningDispatcher(string name, int numberOfThreads)
+        {
+            return this;
+        }
+
+        public CcrSpaceConfig SchedulingWithDispatcherQueue(string dispatcherName)
+        {
+            return this;
+        }
+
+        public CcrSpaceConfig CatchingUnhandledExceptionAt(Action<Exception> defaultExceptionHandler)
+        {
+            return this;
+        }
+                           
+        
+        public CcrSpace Create()
+        {
+            return new CcrSpace(this);
+        }
+    }
+}
