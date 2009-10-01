fa11eba3c5c044d06fa9b68bf4923cc5a4deb0d6
RalfW08 <RalfW08@1f96fe94-a9f5-11de-a86f-b10fb6d3a35f>
2009-10-01 11:55:09 +0000

    moved API classes to a folder of their own. added infrastructure folder.

---
+++ source/CcrSpaces/CcrSpaces.Api/Api/Config/CcrsListenerConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CcrSpaces.Api.Config
+{
+    internal class CcrsListenerConfig<TMessage>
+    {
+        public string Name;
+        public Action<TMessage> MessageHandler;
+        public bool ProcessSequentially;
+    }
+}
