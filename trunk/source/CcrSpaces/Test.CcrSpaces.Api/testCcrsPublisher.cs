f089f2f91cac93a105d9a3d0a2999c0c904340ce
RalfW08 <RalfW08@1f96fe94-a9f5-11de-a86f-b10fb6d3a35f>
2009-10-03 17:37:32 +0000

    publisher tests

---
+++ trunk/source/CcrSpaces/Test.CcrSpaces.Api/testCcrsPublisher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CcrSpaces.Api;
+using NUnit.Framework;
+
+namespace Test.CcrSpaces.Api
+{
+    [TestFixture]
+    public class testCcrsPublisher
+    {
+        [Test]
+        public void Register_subscribers()
+        {
+            var sut = new MockCcrsPublisher<int>();
+            Assert.AreEqual(0, sut.RegisteredSubscribers.Count);
+
+            sut.Subscribe(Console.WriteLine);
+            Assert.AreEqual(1, sut.RegisteredSubscribers.Count);
+
+            var ch = new CcrsOneWayChannel<int>(Console.WriteLine);
+            sut.Subscribe(ch);
+            Assert.AreEqual(2, sut.RegisteredSubscribers.Count);
+            Assert.AreSame(ch, sut.RegisteredSubscribers[1]);
+        }
+
+
+        [Test]
+        public void Unregister_subscribers()
+        {
+            var sut = new MockCcrsPublisher<int>();
+            var handler = new Action<int>(Console.WriteLine);
+            var ch = new CcrsOneWayChannel<int>(Console.WriteLine);
+            sut.Subscribe(handler);
+            sut.Subscribe(ch);
+
+            sut.Unsubscribe(handler);
+            Assert.AreEqual(1, sut.RegisteredSubscribers.Count);
+            Assert.AreSame(ch, sut.RegisteredSubscribers[0]);
+
+            sut.Unsubscribe(handler);
+
+            sut.Unsubscribe(ch);
+            Assert.AreEqual(0, sut.RegisteredSubscribers.Count);
+
+            sut.Unsubscribe(ch);
+        }
+    }
+
+
+    internal class MockCcrsPublisher<T> : CcrsPublisher<T>
+    {
+        public List<ICcrsSimplexChannel<T>> RegisteredSubscribers
+        {
+            get
+            {
+                return base.subscribers;
+            }
+        }
+    }
+}
