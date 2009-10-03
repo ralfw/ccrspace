c15eaad8e3cd117a4c53195596ea47f521a088c5
RalfW08 <RalfW08@1f96fe94-a9f5-11de-a86f-b10fb6d3a35f>
2009-10-03 17:37:16 +0000

    publisher accepts subscriptions publisher can unsubscribe

--- trunk/source/CcrSpaces/CcrSpaces.Api/Api/Publisher.cs
+++ trunk/source/CcrSpaces/CcrSpaces.Api/Api/Publisher.cs
@@ -2,40 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CcrSpaces.Api.Config;
+using Microsoft.Ccr.Core;
 
 namespace CcrSpaces.Api
 {
     public class CcrsPublisher<TBroadcastMessage> : ICcrsSimplexChannel<TBroadcastMessage>
     {
+        private readonly Dictionary<Action<TBroadcastMessage>, ICcrsSimplexChannel<TBroadcastMessage>> subscriptionHandlers;
+        protected readonly List<ICcrsSimplexChannel<TBroadcastMessage>> subscribers;
+
+
+        public CcrsPublisher()
+        {
+            this.subscriptionHandlers = new Dictionary<Action<TBroadcastMessage>, ICcrsSimplexChannel<TBroadcastMessage>>();
+            this.subscribers = new List<ICcrsSimplexChannel<TBroadcastMessage>>();
+        }
+
+
         #region Implementation of ICcrsSimplexChannel<TBroadcastMessage>
         public void Post(TBroadcastMessage message)
         {}
         #endregion
 
 
-        public void Subscribe(Action<TBroadcastMessage> publicationHandler)
-        {}
+        public void Subscribe(Action<TBroadcastMessage> subscriptionHandler)
+        {
+            lock (this.subscriptionHandlers)
+            {
+                if (this.subscriptionHandlers.ContainsKey(subscriptionHandler)) return;
 
-        public void Subscribe(ICcrsSimplexChannel<TBroadcastMessage> publicationListener)
-        {}
+                var cfg = new CcrsOneWayChannelConfig<TBroadcastMessage>
+                              {
+                                  MessageHandler = subscriptionHandler,
+                                  TaskQueue = new DispatcherQueue(),
+                                  ProcessSequentially = true
+                              };
+                var ch = new CcrsOneWayChannel<TBroadcastMessage>(cfg);
+                this.subscriptionHandlers.Add(subscriptionHandler, ch);
+
+                this.Subscribe(ch);
+            }
+        }
+
+        public void Subscribe(ICcrsSimplexChannel<TBroadcastMessage> subscriberChannel)
+        {
+            lock (this.subscribers)
+                this.subscribers.Add(subscriberChannel);
+        }
 
 
-        public void Unsubscribe(Action<TBroadcastMessage> publicationHandler)
-        { }
+        public void Unsubscribe(Action<TBroadcastMessage> subscriptonHandler)
+        {
+            lock(this.subscriptionHandlers)
+            {
+                if (!this.subscriptionHandlers.ContainsKey(subscriptonHandler)) return;
 
-        public void Unsubscribe(ICcrsSimplexChannel<TBroadcastMessage> publicationListener)
-        { }
+                this.Unsubscribe(this.subscriptionHandlers[subscriptonHandler]);
+
+                this.subscriptionHandlers.Remove(subscriptonHandler);
+            }
+        }
+
+        public void Unsubscribe(ICcrsSimplexChannel<TBroadcastMessage> subscriberChannel)
+        {
+            lock(this.subscribers)
+                this.subscribers.Remove(subscriberChannel);
+        }
+
 
         #region Implementation of IPort
 
         public void PostUnknownType(object item)
         {
-            throw new NotImplementedException();
+            this.Post((TBroadcastMessage)item);
         }
 
         public bool TryPostUnknownType(object item)
         {
-            throw new NotImplementedException();
+            if (!(item is TBroadcastMessage)) return false;
+
+            this.PostUnknownType(item);
+            return true;
         }
 
         #endregion
