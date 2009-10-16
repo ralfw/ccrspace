b48bd1f7aef22d19a571d5d6a0b61775b671fa91
RalfW08 <RalfW08@1f96fe94-a9f5-11de-a86f-b10fb6d3a35f>
2009-10-16 06:32:17 +0000

    corrected error in hosting ext methods added usage sample for hosting

--- source/CcrSpaces/CcrSpaces.Hosting/CcrsHostExtensions.cs
+++ source/CcrSpaces/CcrSpaces.Hosting/CcrsHostExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using CcrSpaces.Channels;
+﻿using CcrSpaces.Channels;
 using CcrSpaces.Core;
 using Microsoft.Ccr.Core;
 
@@ -20,12 +16,12 @@ namespace CcrSpaces.Hosting
         public static void HostPort<T>(this ICcrSpace space, Port<T> port, string name) { space.Resolve<CcrsHost>().HostPort(port, name); }
         public static void HostPort<TInput, TOutput>(this ICcrSpace space, PortSet<TInput, CcrsRequest<TInput, TOutput>> ports, string name) { space.Resolve<CcrsHost>().HostPort(ports, name); }
         public static void HostAsyncComponent<TAsyncContract>(this ICcrSpace space, TAsyncContract worker, string name)
-            where TAsyncContract : IPort { space.HostAsyncComponent(worker, name); }
+            where TAsyncContract : IPort { space.Resolve<CcrsHost>().HostAsyncComponent(worker, name); }
 
 
         public static Port<T> ConnectToPort<T>(this ICcrSpace space, string remoteAddress) { return space.Resolve<CcrsHost>().ConnectToPort<T>(remoteAddress); }
-        public static PortSet<TInput, CcrsRequest<TInput, TOutput>> ConnectToPort<TInput, TOutput>(this ICcrSpace space, string remoteAddress) { return space.ConnectToPort<PortSet<TInput, CcrsRequest<TInput, TOutput>>>(remoteAddress); }
+        public static PortSet<TInput, CcrsRequest<TInput, TOutput>> ConnectToPort<TInput, TOutput>(this ICcrSpace space, string remoteAddress) { return space.Resolve<CcrsHost>().ConnectToPort<TInput, TOutput>(remoteAddress); }
         public static TAsyncContract ConnectToAsyncComponent<TAsyncContract>(this ICcrSpace space, string remoteAddress)
-            where TAsyncContract : IPort, new() { return space.ConnectToAsyncComponent<TAsyncContract>(remoteAddress); }
+            where TAsyncContract : IPort, new() { return space.Resolve<CcrsHost>().ConnectToAsyncComponent<TAsyncContract>(remoteAddress); }
     }
 }
---
+++ source/CcrSpaces/UsageSamples.CcrSpace/useHosting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+using CcrSpaces.Core;
+using CcrSpaces.Channels;
+using CcrSpaces.Hosting;
+
+namespace UsageSamples.CcrSpaces
+{
+    [TestFixture]
+    public class useHosting
+    {
+        [Test]
+        public void Echo_server()
+        {
+            using(var server = new CcrSpace().ConfigureAsHost("tcp.port=9999"))
+            {
+                var echoService = server.CreateChannel<string, string>(EchoProcessor);
+                server.HostPort(echoService, "echoservice");
+
+                using(var client = new CcrSpace().ConfigureAsHost("tcp.port=0"))
+                {
+                    var remoteEchoService = client.ConnectToPort<string, string>("localhost:9999/echoservice");
+                    remoteEchoService
+                        .Request("hello, world!")
+                        .Receive(s => Console.WriteLine("echo received: '{0}'", s));
+
+                    Thread.Sleep(2000);
+                }
+            }
+        }
+
+
+        private static string EchoProcessor(string text)
+        {
+            Console.WriteLine("  request to echo back '{0}'", text);
+            return text.ToUpper();
+        }
+    }
+}
