139eda2bff47d86b18181554b84b82c766cf1d69
RalfW08 <RalfW08@1f96fe94-a9f5-11de-a86f-b10fb6d3a35f>
2009-10-30 22:09:26 +0000

    flows can be built stage by stage

---
+++ source/CcrSpaces/CcrSpaces.Flows/OneWayFlow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CcrSpaces.Flows.Infrastructure;
+using CcrSpaces.Flows.Stages;
+using Microsoft.Ccr.Core;
+
+namespace CcrSpaces.Flows
+{
+    internal class CcrsFlow<TInput> : Port<TInput>
+    {
+        private readonly StageBase firstStage;
+
+
+        internal CcrsFlow(StageBase firstStage)
+        {
+            this.firstStage = firstStage;
+            this.RegisterGenericSyncReceiver(msg => this.firstStage.Post(new StageMessage { Message = msg }));
+        }
+
+
+        public CcrsFlow(Action<TInput> handler)
+        {
+            this.firstStage = new TerminalStage<TInput>(handler);
+            this.RegisterGenericSyncReceiver(msg => this.firstStage.Post(new StageMessage { Message = msg }));
+        }
+    }
+}
---
+++ source/CcrSpaces/CcrSpaces.Flows/RequestResponseFlow.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CcrSpaces.Channels;
+using CcrSpaces.Flows.Infrastructure;
+using CcrSpaces.Flows.Stages;
+using Microsoft.Ccr.Core;
+
+namespace CcrSpaces.Flows
+{
+    internal class CcrsFlow<TInput, TOutput> : Port<CcrsRequest<TInput, TOutput>>
+    {
+        private readonly StageBase firstStage;
+        private StageBase lastStage;
+
+
+        public CcrsFlow(Action<TInput, Port<TOutput>> handler)
+        {
+            this.firstStage = new IntermediateStage<TInput, TOutput>(handler);
+            this.lastStage = this.firstStage;
+            WireUpRequestPort();
+        }
+
+
+        public CcrsFlow(StageBase firstStage, StageBase lastStage)
+        {
+            this.firstStage = firstStage;
+            this.lastStage = lastStage;
+            WireUpRequestPort();
+        }
+
+
+        private void WireUpRequestPort()
+        {
+            this.RegisterGenericSyncReceiver(msg =>
+            {
+                var req = (CcrsRequest<TInput, TOutput>)msg;
+                this.firstStage.Post(new StageMessage { Message = req.Request, ResponsePort = req.Responses });
+            });
+        }
+
+
+        public CcrsFlow<TInput, TNextOutput> Continue<TNextOutput>(Action<TOutput, Port<TNextOutput>> intermediateHandler)
+        {
+            this.lastStage.Next = new IntermediateStage<TOutput, TNextOutput>(intermediateHandler);
+            this.lastStage = this.lastStage.Next;
+            return new CcrsFlow<TInput, TNextOutput>(this.firstStage, this.lastStage);
+        }
+
+
+        public CcrsFlow<TInput> Finish(Action<TOutput> terminalHandler)
+        {
+            this.lastStage.Next = new TerminalStage<TOutput>(terminalHandler);
+            return new CcrsFlow<TInput>(this.firstStage);
+        }
+    }
+}
