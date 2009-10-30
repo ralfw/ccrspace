423426805c738f98df769e7a3188b1ed878db226
RalfW08 <RalfW08@1f96fe94-a9f5-11de-a86f-b10fb6d3a35f>
2009-10-30 18:15:10 +0000

    introduced internal flow stages as building blocks for flows

---
+++ source/CcrSpaces/CcrSpaces.Flows/Stages/IntermediateStage_Details.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Ccr.Core;
+
+namespace CcrSpaces.Flows.Stages
+{
+    partial class IntermediateStage<TInput, TOutput>
+    {
+        private void AssertHasResponsePort(StageMessage m)
+        {
+            if (base.Next == null && m.ResponsePort == null) throw new InvalidOperationException("Stage cannot process message due to missing next stage and no response port being defined!");
+        }
+
+        private bool IsTerminalStage()
+        {
+            return base.Next == null;
+        }
+
+        private void SendResultBackToCaller(StageMessage m, object r)
+        {
+            m.ResponsePort.PostUnknownType(r);
+        }
+
+        private void PassResultOnToNextStage(StageMessage m, object r)
+        {
+            base.Next.Post(new StageMessage
+            {
+                Message = r,
+                ResponsePort = m.ResponsePort
+            });
+        }
+
+        private void ProcessMessageAndReturnResult(Action<TInput, Port<TOutput>> handler, TInput msg, Port<TOutput> responsePort)
+        {
+            handler(msg, responsePort);
+        }
+    }
+}
---
+++ source/CcrSpaces/CcrSpaces.Flows/Stages/TerminalStage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CcrSpaces.Channels;
+
+namespace CcrSpaces.Flows.Stages
+{
+    internal class TerminalStage<TInput> : StageBase
+    {
+        public TerminalStage(Action<TInput> handler)
+        {
+            base.Configure(new CcrsOneWayChannelConfig<StageMessage>
+                                {
+                                    MessageHandler = m => ConsumeMessage(handler, (TInput)m.Message),
+                                });
+        }
+
+
+        private void ConsumeMessage(Action<TInput> handler, TInput msg)
+        {
+            handler(msg);
+        }
+    }
+}
